7555be2403a0c51f5fa7e55bbda31bd045fefeca
Bandla Ganesh <125232544+Ganesh9951@users.noreply.github.com>
2025-08-09 09:47:34 +0530

    Create program-1.R

---
+++ program-1.R
@@ -0,0 +1,7 @@
+name <- readline(prompt = "Enter your name: ")
+age <- as.numeric(readline(prompt = "Enter your age: "))
+cat("Hello", name, "!\n")
+cat("You are", age, "years old\n")
+cat("R version into:\n")
+print(R.version.string)
+  
